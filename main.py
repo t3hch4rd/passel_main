44a7e3471fbe63477befddadd18e1ce284ec987c
t3hch4rd <71416289+t3hch4rd@users.noreply.github.com>
2022-09-21 16:38:27 -0400

    Update main.py
    removed provisions for hosting on heroku.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from random import randrange
 
-# TODO if using Github diff deployment on HeroKu uncomment the next line
 import os
 import discord
 from discord.ext import commands
@@ -207,9 +206,4 @@ async def on_guild_channel_pins_update(channel, last_pin):
     except:
         print("unpinned a message, not useful for bot so does nothing")
 
-
-# TODO Replace TOKEN with the token from discord developer portal 
-#client.run('MTAwOTEyNjIzMTU4Njg5ODAyMg.GOPYKu.B3uxCiiU9IEOp3mWgoPe5Rq6b6w5Ynptc25Ews')
-
-# TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
 client.run(os.environ.get('TOKEN'))
